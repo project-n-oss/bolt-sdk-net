5ca49264e99f39085ce4f8a84a36f34b5abaa7c8
Sasha Joseph <sasha.joseph@gmail.com>
2020-05-27 12:24:04 -0700

    build: initial commit of Project N Bolt SDK for .NET

---
+++ BoltS3Client.cs
@@ -0,0 +1,195 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.S3;
+using System.Configuration;
+
+namespace ProjectN.Bolt
+{
+    /// <summary>
+    /// Implementation for accessing S3 via Bolt.
+    ///
+    /// Provides the same constructors as AmazonS3Client, but always resolves to the Bolt service URL
+    /// as specified in app settings.
+    ///
+    /// Example App.config:
+    /// <code>
+    /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+    /// &lt;configuration&gt;
+    ///     &lt;appSettings&gt;
+    ///         &lt;add key="BoltURL" value="http://bolt.project.n"/&gt;
+    ///     &lt;/appSettings&gt;
+    /// &lt;/configuration&gt;
+    /// </code>
+    /// </summary>
+    public class BoltS3Client : AmazonS3Client
+    {
+        private static readonly string BoltServiceUrl = ConfigurationManager.AppSettings.Get("BoltURL");
+
+        private static readonly AmazonS3Config BoltConfig = new AmazonS3Config
+        {
+            ServiceURL = BoltServiceUrl
+        };
+
+        /// <summary>
+        /// Constructs AmazonS3Client with the credentials loaded from the application's
+        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
+        ///
+        /// Example App.config with credentials set.
+        /// <code>
+        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+        /// &lt;configuration&gt;
+        ///     &lt;appSettings&gt;
+        ///         &lt;add key="AWSProfileName" value="AWS Default"/&gt;
+        ///     &lt;/appSettings&gt;
+        /// &lt;/configuration&gt;
+        /// </code>
+        ///
+        /// </summary>
+        public BoltS3Client() : base(BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with the credentials loaded from the application's
+        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
+        ///
+        /// Example App.config with credentials set.
+        /// <code>
+        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+        /// &lt;configuration&gt;
+        ///     &lt;appSettings&gt;
+        ///         &lt;add key="AWSProfileName" value="AWS Default"/&gt;
+        ///     &lt;/appSettings&gt;
+        /// &lt;/configuration&gt;
+        /// </code>
+        ///
+        /// </summary>
+        /// <param name="region">The region to connect.</param>
+        public BoltS3Client(RegionEndpoint region) : base(BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with the credentials loaded from the application's
+        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
+        ///
+        /// Example App.config with credentials set.
+        /// <code>
+        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+        /// &lt;configuration&gt;
+        ///     &lt;appSettings&gt;
+        ///         &lt;add key="AWSProfileName" value="AWS Default"/&gt;
+        ///     &lt;/appSettings&gt;
+        /// &lt;/configuration&gt;
+        /// </code>
+        ///
+        /// </summary>
+        /// <param name="config">The AmazonS3Client Configuration Object</param>
+        public BoltS3Client(AmazonS3Config config) : base(config)
+        {
+            config.ServiceURL = BoltServiceUrl;
+        }
+
+        /// <summary>Constructs AmazonS3Client with AWS Credentials</summary>
+        /// <param name="credentials">AWS Credentials</param>
+        public BoltS3Client(AWSCredentials credentials) : base(credentials, BoltConfig)
+        {
+        }
+
+        /// <summary>Constructs AmazonS3Client with AWS Credentials</summary>
+        /// <param name="credentials">AWS Credentials</param>
+        /// <param name="region">The region to connect.</param>
+        public BoltS3Client(AWSCredentials credentials, RegionEndpoint region) : base(credentials, BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Credentials and an
+        /// AmazonS3Client Configuration object.
+        /// </summary>
+        /// <param name="credentials">AWS Credentials</param>
+        /// <param name="clientConfig">The AmazonS3Client Configuration Object</param>
+        public BoltS3Client(AWSCredentials credentials, AmazonS3Config clientConfig) : base(credentials, clientConfig)
+        {
+            clientConfig.ServiceURL = BoltServiceUrl;
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Access Key ID and AWS Secret Key
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        public BoltS3Client(string awsAccessKeyId, string awsSecretAccessKey) : base(awsAccessKeyId, awsSecretAccessKey,
+            BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Access Key ID and AWS Secret Key
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        /// <param name="region">The region to connect.</param>
+        public BoltS3Client(string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region) : base(
+            awsAccessKeyId, awsSecretAccessKey, BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Access Key ID, AWS Secret Key and an
+        /// AmazonS3Client Configuration object.
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        /// <param name="clientConfig">The AmazonS3Client Configuration Object</param>
+        public BoltS3Client(string awsAccessKeyId, string awsSecretAccessKey, AmazonS3Config clientConfig) : base(
+            awsAccessKeyId, awsSecretAccessKey, clientConfig)
+        {
+            clientConfig.ServiceURL = BoltServiceUrl;
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Access Key ID and AWS Secret Key
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        /// <param name="awsSessionToken">AWS Session Token</param>
+        public BoltS3Client(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken) : base(
+            awsAccessKeyId, awsSecretAccessKey, awsSessionToken, BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Access Key ID and AWS Secret Key
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        /// <param name="awsSessionToken">AWS Session Token</param>
+        /// <param name="region">The region to connect.</param>
+        public BoltS3Client(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken,
+            RegionEndpoint region) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, BoltConfig)
+        {
+        }
+
+        /// <summary>
+        /// Constructs AmazonS3Client with AWS Access Key ID, AWS Secret Key and an
+        /// AmazonS3Client Configuration object.
+        /// </summary>
+        /// <param name="awsAccessKeyId">AWS Access Key ID</param>
+        /// <param name="awsSecretAccessKey">AWS Secret Access Key</param>
+        /// <param name="awsSessionToken">AWS Session Token</param>
+        /// <param name="clientConfig">The AmazonS3Client Configuration Object</param>
+        public BoltS3Client(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken,
+            AmazonS3Config clientConfig) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, clientConfig)
+        {
+            clientConfig.ServiceURL = BoltServiceUrl;
+        }
+
+        /// <summary>Creates the signer for the service.</summary>
+        protected override AbstractAWSSigner CreateSigner()
+        {
+            return new BoltSigner();
+        }
+    }
+}
---
+++ BoltSigner.cs
@@ -0,0 +1,73 @@
+using System;
+using Amazon;
+using Amazon.SecurityToken.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Runtime.Internal.Auth;
+using Amazon.Runtime.Internal.Util;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model.Internal.MarshallTransformations;
+
+namespace ProjectN.Bolt
+{
+    /// <summary>
+    /// AWS4 protocol signer for Bolt.
+    /// Rather than directly signing S3 requests, we instead use credentials sent into the S3 request,
+    /// but proxy the signature to a canonical STS GetCallerIdentity API call.
+    /// </summary>
+    public class BoltSigner : AWS4Signer
+    {
+        private static readonly Uri StsEndpoint = new Uri("https://sts.amazonaws.com/");
+
+        private static readonly IClientConfig StsConfig = new AmazonSecurityTokenServiceConfig
+        {
+            RegionEndpoint = RegionEndpoint.USEast1
+        };
+
+        /// <summary>
+        /// Calculates and signs the specified request using the AWS4 signing protocol by using the
+        /// AWS account credentials given in the method parameters. The resulting signature is added
+        /// to the request headers as 'Authorization'. Parameters supplied in the request, either in
+        /// the resource path as a query string or in the Parameters collection must not have been
+        /// uri encoded. If they have, use the SignRequest method to obtain a signature.
+        /// </summary>
+        /// <param name="request">
+        /// The request to compute the signature for. Additional headers mandated by the AWS4 protocol
+        /// ('host' and 'x-amz-date') will be added to the request before signing.
+        /// </param>
+        /// <param name="clientConfig">
+        /// Client configuration data encompassing the service call (notably authentication
+        /// region, endpoint and service name).
+        /// </param>
+        /// <param name="metrics">Metrics for the request</param>
+        /// <param name="awsAccessKeyId">
+        /// The AWS public key for the account making the service call.
+        /// </param>
+        /// <param name="awsSecretAccessKey">
+        /// The AWS secret key for the account making the call, in clear text.
+        /// </param>
+        /// <exception cref="T:Amazon.Runtime.SignatureException">
+        /// If any problems are encountered while signing the request.
+        /// </exception>
+        public override void Sign(IRequest request, IClientConfig clientConfig, RequestMetrics metrics,
+            string awsAccessKeyId, string awsSecretAccessKey)
+        {
+            // Create the canonical STS request to get caller identity, with session token if appropriate.
+            var iamRequest = GetCallerIdentityRequestMarshaller.Instance.Marshall(new GetCallerIdentityRequest());
+            iamRequest.Headers["User-Agent"] = request.Headers["User-Agent"];
+            iamRequest.Endpoint = StsEndpoint;
+            if (request.Headers.TryGetValue("X-Amz-Security-Token", out var sessionToken))
+            {
+                iamRequest.Headers["X-Amz-Security-Token"] = sessionToken;
+            }
+
+            AWS4SigningResult awS4SigningResult =
+                SignRequest(iamRequest, StsConfig, metrics, awsAccessKeyId,
+                    awsSecretAccessKey);
+            // On the receiving end, Bolt should forward these request headers to the STS GetCallerIdentity API.
+            request.Headers["X-Amz-Content-SHA256"] = iamRequest.Headers["X-Amz-Content-SHA256"];
+            request.Headers["X-Amz-Date"] = iamRequest.Headers["X-Amz-Date"];
+            request.Headers["Authorization"] = awS4SigningResult.ForAuthorizationHeader;
+        }
+    }
+}
